92ef06802d16771d7e118e55de18dd0bd351f9ef
zheyanliu99 <zl3119@columbia.edu>
2022-03-31 16:57:59 -0400

    add event info

--- flaskr/event.py
+++ flaskr/event.py
@@ -31,7 +31,7 @@ def book():
     user_id = session["user_id"]
 
     if form.validate_on_submit():
-        flash('Here is all the available events')
+        # flash('Here is all the available events')
         # This try except is not necessary, can not work similarly as the routine
         try:
             form.validate_date(form.startdate, form.enddate)
@@ -70,22 +70,63 @@ def book():
             
             cur.execute(sql, (user_id, startdate, enddate))
             res = cur.fetchall()
-            if res:
+            if not res:
+                flash("No available event, try another date")
+            else:
                 headings = heading_from_dict(res)
 
-            if request.form.get("bookbutton"):
-                event_id = request.form.get('bookbutton')
-                print(event_id, user_id)
-                try:
-                    cur.execute(
-                        "INSERT INTO event_appointment (user_id, event_id) VALUES (%s, %s)",
-                        (user_id, event_id),
-                    )
-                    db.commit()
-                except Exception as e:
-                    # The username was already taken, which caused the
-                    # commit to fail. Show a validation error.
-                    print(e)
-                    flash("Book failure, you have booked this event appointment before")
+                if request.form.get("bookbutton"):
+                    event_id = request.form.get('bookbutton')
+                    print(event_id, user_id)
+                    try:
+                        cur.execute(
+                            "INSERT INTO event_appointment (user_id, event_id) VALUES (%s, %s)",
+                            (user_id, event_id),
+                        )
+                        db.commit()
+                    except Exception as e:
+                        # The username was already taken, which caused the
+                        # commit to fail. Show a validation error.
+                        print(e)
+                        flash("Book failure, you have booked this event appointment before")
+
+                if request.form.get("more"):
+                    event_id = request.form.get('more')
+                    return redirect(url_for('event.event_info', event_id=event_id))
                 
     return render_template('event/book.html', form=form, headings=headings, res=res)
+
+
+@bp.route('/<int:event_id>')
+def event_info(event_id):
+    # grab the requested blog post by id number or return 404
+    db, cur = get_db()
+    sql = """
+        WITH event_selected as (
+        SELECT * 
+        FROM event 
+        WHERE event_id = %s)
+
+        SELECT b.description, p.place_name place, b.starttime, b.endtime, d.nickname coach_name, a.num_of_users participaters, b.classlimit
+        FROM
+        (
+        SELECT t1.event_id, count(distinct user_id)  as num_of_users
+        FROM event_selected t1
+        LEFT JOIN event_appointment t2
+        ON t1.event_id = t2.event_id
+        GROUP BY t1.event_id) a
+        INNER JOIN event_selected b
+        ON a.event_id = b.event_id 
+        INNER JOIN coach c
+        ON b.coach_id = c.coach_id
+        INNER JOIN users d
+        ON c.user_id = d.user_id
+        INNER JOIN place p
+        ON b.place_id = p.place_id
+        ORDER BY b.starttime"""
+            
+    cur.execute(sql, (event_id,))
+    events = cur.fetchone()
+    event_headings = [key for key in events.keys()]
+    events_res = [(event, event_heading) for (event, event_heading) in zip(events, event_headings)]
+    return render_template('event/event.html', events_res=events_res)
