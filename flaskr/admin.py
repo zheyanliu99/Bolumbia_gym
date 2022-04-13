5cc20d4fcc1918f7bca4f4dc70ade6753a1a5823
huangzhuolun <74016311+huangzhuolun@users.noreply.github.com>
2022-04-13 17:13:28 -0400

    update admin part only for admin Q&A

--- flaskr/admin.py
+++ flaskr/admin.py
@@ -12,12 +12,13 @@ from flask import url_for
 from flask_restful import abort
 from marshmallow import ValidationError
 import sys
-import logging 
+import logging
 logging.basicConfig(level=logging.DEBUG)
 
 from db import get_db
 
 from forms.event import SearchEventForm, CreateEventForm, SearchTimeForm
+from forms.QA import Q_AAnswer, Q_Aform
 
 bp = Blueprint("admin", __name__, url_prefix="/admin")
 
@@ -46,32 +47,32 @@ def eventapprove():
     sql = """
         WITH conflict_events as(
         SELECT a.event_id
-        FROM     
-        (
-        SELECT * 
-        FROM event 
-        WHERE event_id not in (SELECT event_id FROM event_approve)) a, 
-        (
-        SELECT t2.* 
-        FROM event_approve t1
-        INNER JOIN  event t2
-        ON t1.event_id = t2.event_id) b
-        WHERE a.event_id != b.event_id 
-        AND a.date = b.date
-        AND ((a.endtime > b.starttime AND a.endtime <= b.endtime) OR (a.starttime >= b.starttime AND a.starttime < b.endtime)))
-
-        SELECT d.username coach_name, a.description, starttime, endtime, place_name, classlimit, ageconstraint_lower, ageconstraint_upper, capacity, c.user_id, a.event_id 
         FROM
         (
         SELECT *
-        FROM event 
+        FROM event
+        WHERE event_id not in (SELECT event_id FROM event_approve)) a,
+        (
+        SELECT t2.*
+        FROM event_approve t1
+        INNER JOIN  event t2
+        ON t1.event_id = t2.event_id) b
+        WHERE a.event_id != b.event_id
+        AND a.date = b.date
+        AND ((a.endtime > b.starttime AND a.endtime <= b.endtime) OR (a.starttime >= b.starttime AND a.starttime < b.endtime)))
+
+        SELECT d.username coach_name, a.description, starttime, endtime, place_name, classlimit, ageconstraint_lower, ageconstraint_upper, capacity, c.user_id, a.event_id
+        FROM
+        (
+        SELECT *
+        FROM event
         WHERE event_id not in (SELECT event_id FROM event_approve)
         AND event_id not in (SELECT event_id FROM conflict_events)) a
         INNER JOIN place p
         ON a.place_id = p.place_id
-        INNER JOIN coach c 
+        INNER JOIN coach c
         ON a.coach_id = c.coach_id
-        INNER JOIN users d 
+        INNER JOIN users d
         ON c.user_id = d.user_id
         ORDER BY starttime
     """
@@ -108,15 +109,15 @@ def eventshutdown():
             pass
         else:
             startdate = timeform.startdate.data
-            # startdate = max(datetime.date.today(), startdate) 
+            # startdate = max(datetime.date.today(), startdate)
             # timeform.startdate.data = startdate
             enddate = timeform.enddate.data
             print(startdate, enddate)
             db, cur = get_db()
             sql = """
             WITH event_selected as (
-            SELECT * 
-            FROM event 
+            SELECT *
+            FROM event
             WHERE event_id in (SELECT event_id FROM event_approve)
             AND date >= %s
             AND date <= %s)
@@ -130,7 +131,7 @@ def eventshutdown():
             ON t1.event_id = t2.event_id
             GROUP BY t1.event_id) a
             INNER JOIN event_selected b
-            ON a.event_id = b.event_id 
+            ON a.event_id = b.event_id
             INNER JOIN coach c
             ON b.coach_id = c.coach_id
             INNER JOIN users d
@@ -138,7 +139,7 @@ def eventshutdown():
             WHERE num_of_users < classlimit
             ORDER BY b.starttime"""
 
-                    
+
             cur.execute(sql, (startdate, enddate))
             approved_events = cur.fetchall()
             if request.form.get("shutdownbutton"):
@@ -152,3 +153,104 @@ def eventshutdown():
                     return redirect(url_for('admin.eventshutdown'))
 
     return render_template("admin/eventshutdown.html", approved_events=approved_events, timeform=timeform)
+
+@bp.route("/adminseeQ&A")
+def indexQA():
+    form = Q_AAnswer()
+    user_id = session["user_id"]
+    db, cur = get_db()
+    cur.execute("""
+               SELECT r.*, u.username raisername, u2.username answerername, a.admin_id, a.answer_content
+               FROM raise_question r
+               INNER JOIN users u
+               ON u.user_id = r.user_id
+               LEFT JOIN answer a
+               ON r.questiontitle_id = a.questiontitle_id
+               LEFT JOIN users u2
+               ON a.admin_id = u2.user_id
+               ORDER BY r.raisedate DESC""")
+    Q_A = cur.fetchall()
+
+    cur.execute("""
+                SELECT a.user_id, a.admin_id
+                From admin a LEFT JOIN users u
+                ON u.user_id = a.user_id""")
+    admin = cur.fetchall()
+
+    return render_template("admin/QAindex.html", user_id = user_id, Q_A = Q_A, form = form, admin = admin)
+
+
+# create answer
+@bp.route("/answer/<int:questiontitle_id>", methods=("GET", "POST"))
+def answer(questiontitle_id):
+    form = Q_AAnswer()
+    user_id = session["user_id"]
+    db, cur = get_db()
+
+    cur.execute("SELECT * From answer WHERE questiontitle_id = %s", (questiontitle_id,))
+    answer = cur.fetchone()
+
+    if answer is None:
+        cur.execute("SELECT admin_id From admin WHERE user_id = %s", (user_id,))
+        admin_id = cur.fetchone()['admin_id']
+        if admin_id is None:
+            flash("Only admin can answer the question!")
+        else:
+            if form.validate_on_submit():
+                Answer = form.Answer.data
+                date = datetime.datetime.now()
+                cur.execute(
+                    "INSERT INTO answer (questiontitle_id, answer_content, answer_time, admin_id) VALUES (%s, %s, %s, %s)",
+                    (questiontitle_id, Answer, date, admin_id),
+                )
+                db.commit()
+                return redirect(url_for("admin.indexQA"))
+    else:
+        flash("You can only answer once, you can edit your answer to update it!")
+
+    return render_template("admin/QAanswer.html", answer = answer, questiontitle_id = questiontitle_id, form = form, user_id = user_id)
+
+
+#edit the answer
+@bp.route("/editanswer/<int:questiontitle_id>", methods=("GET", "POST"))
+def edit(questiontitle_id):
+    user_id = session["user_id"]
+    db, cur = get_db()
+
+    cur.execute("SELECT * From answer WHERE questiontitle_id = %s", (questiontitle_id,))
+    Answer = cur.fetchone()
+
+    form = Q_AAnswer(Answer = Answer['answer_content'], date = Answer['answer_time'])
+
+    if Answer == None:
+        flash("Please Anwser first then update!")
+    else:
+        if form.validate_on_submit():
+            Answer = form.Answer.data
+            date = datetime.datetime.now()
+            sql = """
+                UPDATE answer
+                SET answer_content = %s, answer_time = %s
+                WHERE questiontitle_id = %s
+            """
+            cur.execute(sql, (Answer, date, questiontitle_id))
+            db.commit()
+            flash("updated your answer!")
+            return redirect(url_for('admin.indexQA'))
+
+    return render_template('admin/QAanswer.html', user_id = user_id, questiontitle_id = questiontitle_id, form = form)
+
+# delete the answer
+@bp.route("/<int:questiontitle_id>/deleteanswer", methods=("POST",))
+def deleteanswer(questiontitle_id):
+    user_id = session["user_id"]
+    db, cur= get_db()
+
+    sql = """
+        DELETE FROM answer WHERE questiontitle_id = %s
+    """
+    cur.execute(sql, (questiontitle_id, ))
+    db.commit()
+
+    flash("Deleted your answer!")
+    return redirect(url_for("admin.indexQA"))
