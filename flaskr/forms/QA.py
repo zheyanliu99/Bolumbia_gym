7e668f03a6c8b9f9f0c39ebbcda8c923b4b61afc
zheyanliu99 <zl3119@columbia.edu>
2022-04-14 14:14:48 -0400

    change question and answer length to both 200

--- flaskr/forms/QA.py
+++ flaskr/forms/QA.py
@@ -10,11 +10,11 @@ import datetime
 
 class Q_Aform(FlaskForm):
     title = StringField('Title', validators=(DataRequired(),))
-    content = TextAreaField('Question', validators=(DataRequired(), length(max=500), ))
+    content = TextAreaField('Question', validators=(DataRequired(), length(max=200), ))
 #    date = DateField('raisedate', format='%Y-%m-%d', validators=(DataRequired(),))
     submit = SubmitField('Sumbit Question')
 
 class Q_AAnswer(FlaskForm):
-    Answer = TextAreaField('Answer', validators=(DataRequired(),))
+    Answer = TextAreaField('Answer', validators=(DataRequired(), length(max=200), ))
 #    date = DateField('raisedate', format='%Y-%m-%d', validators=(DataRequired(),))
     submit = SubmitField('Sumbit')
