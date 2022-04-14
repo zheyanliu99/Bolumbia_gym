58c93c83507f0b72d138d77ab471911e9c1be059
zheyanliu99 <zl3119@columbia.edu>
2022-04-14 14:16:33 -0400

    change post length to 200, title 100

--- flaskr/forms/post.py
+++ flaskr/forms/post.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField, DateField, TextAreaField, RadioField
-from wtforms.validators import DataRequired,Email,EqualTo
+from wtforms.validators import DataRequired,Email,EqualTo, length
 from wtforms import ValidationError
 from flask_wtf.file import FileField, FileAllowed
 from flask import flash
@@ -9,15 +9,15 @@ import datetime
 
 
 class Postform(FlaskForm):
-    title = StringField('title', validators=(DataRequired(),))
-    post = TextAreaField('post', validators=(DataRequired(),))
+    title = StringField('title', validators=(DataRequired(), length(max=100), ))
+    post = TextAreaField('post', validators=(DataRequired(), length(max=200), ))
     open_to = SelectField(u'open_to', choices=[('everyone', 'everyone'), ('followers', 'only followers'), ('myself', 'just myself')], validators=(DataRequired(),))
     # date = DateField('Date', format='%Y-%m-%d', validators=(DataRequired(),))
     submit = SubmitField('sumbit posts')
 
 class Commentform(FlaskForm):
     anonymous = RadioField('anonymous ', choices=[(False,'Unanonymous'),(True,'Anonymous')], default=False)
-    comment = TextAreaField('post', validators=(DataRequired(),))
+    comment = TextAreaField('post', validators=(DataRequired(), length(max=200), ))
     submit = SubmitField('sumbit conmment')
 
 class Likeform(FlaskForm):
